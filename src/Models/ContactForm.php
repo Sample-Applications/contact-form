7f8f4066227514b8a71c81576c6a31ceef8ad09e
kenprogrammer <kenprogrammer@gmail.com>
2021-12-08 12:33:18 +0300

    My First Package

---
+++ src/Models/ContactForm.php
@@ -0,0 +1,9 @@
+<?php
+    namespace Kenprogrammer\Contactform\Models;
+    use Illuminate\Database\Eloquent\Model;
+    
+    class ContactForm extends Model
+    {
+        protected $guarded = [];
+        protected $table = 'contact';
+    }
