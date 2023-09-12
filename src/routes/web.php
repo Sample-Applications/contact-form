68ebac4cc0ac02f61d42371953fc0e5a822d0357
kenprogrammer <kenprogrammer@gmail.com>
2023-09-12 15:11:38 +0300

    Update routes to follow the new format

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
+    use Kenprogrammer\Contactform\Http\Controllers\ContactFormController;
+
     Route::group(['namespace' => 'Kenprogrammer\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
-        Route::get('contact', 'ContactFormController@index');
-        Route::post('contact', 'ContactFormController@sendMail')->name('contact');
+        Route::get('contact', [ContactFormController::class,'index']);
+        Route::post('contact', [ContactFormController::class,'sendMail'])->name('contact');
     });
 ?>
